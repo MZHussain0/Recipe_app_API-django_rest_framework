5faed150db9a2833ebcdd36f772f46dcb7b178b5
Zakir Hussain <mzhussaino92@gmail.com>
2019-07-24 21:17:38 +0530

    Re-factored recipe views

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,36 +5,30 @@ from .serializers import TagSerializer, IngredientSerializer
 from core.models import Tag, Ingredient
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """ Manage Tags in the database """
+class BaseRecipeAttr(viewsets.GenericViewSet,
+                     mixins.CreateModelMixin, mixins.ListModelMixin):
+    """ Basic viewset for user owned recipe attributes"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """ Return the object for the authenticatd user """
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """ Create a new tag """
+
+        serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseRecipeAttr):
+    """ Manage Tags in the database """
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
-    def get_queryset(self):
-        """ Return objects for the current authenticated user only """
-        return self.queryset.filter(user=self.request.user).order_by('-name')
 
-    def perform_create(self, serializer):
-        """ Create a new tag """
-        serializer.save(user=self.request.user)
-
-
-class IngredientViewset(viewsets.GenericViewSet, mixins.ListModelMixin,
-                        mixins.CreateModelMixin):
+class IngredientViewset(BaseRecipeAttr):
     """ Manage ingredients in the database """
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-
-    def get_queryset(self):
-        """ Return objects for the current authenticated user only """
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """ Create a new tag """
-        serializer.save(user=self.request.user)
